ca84dd3a8162695313c94df3bd175ac114919b78
Heyriim <37863630+Heyriim@users.noreply.github.com>
2019-10-31 10:25:20 +0100

    Nuc à deux points

--- Be_cameraThermique.py
+++ Be_cameraThermique.py
@@ -18,11 +18,33 @@ Created on Thu Oct 31 09:27:50 2019
 import numpy as np
 import matplotlib.pyplot as plot
 
+
 if __name__ == "__main__" :
     
-    
+    # question 1 : affichage image
     data = np.loadtxt('row_image.dat')
     plot.figure(1)
     plot.subplot(2,2,1)
-    plot.imshow(data)
+    plot.imshow(data, vmin = 4000, vmax = 7000)
+    plot.title("Image brute")
+    plot.colorbar()
+    
+    # question 2 : choisir deux températures + calculer NUC à deux points
+    T1 = np.loadtxt('temp_bb_18C.dat')
+    T2 = np.loadtxt('temp_bb_26C.dat')
+    
+    alpha =  (np.mean(T1)-np.mean(T2))/(T1-T2)
+    
+    plot.subplot(2,2,2)
+    plot.imshow(alpha)
+    plot.title("Matrice de gain")
+    plot.colorbar()
+    
+    beta =  np.mean(T1)-alpha*T1
+    
+    plot.subplot(2,2,3)
+    plot.imshow(beta)
+    plot.title("Matrice d'offset")
+    plot.colorbar()
+    
     
