6df396ac5ce5699e4f627caa47332659340e6fe1
Heyriim <37863630+Heyriim@users.noreply.github.com>
2019-10-31 09:49:57 +0100

    Affichage de l'image brute

--- Be_cameraThermique.py
+++ Be_cameraThermique.py
@@ -4,16 +4,25 @@ Created on Thu Oct 31 09:27:50 2019
 
 @author: denes
 """
+#
+#Afficher le contenu d’une matrice sous la forme d’une image
+#
+#plot.figure(1)
+#plot.imshow(data, vmin=25, vmax=35)
+#plot.colorbar()
+#plot.show()
+#Quelques fonctions utiles
+#scipy.optimize.curve_fit
+#numpy.where
 
-import numpy
+import numpy as np
 import matplotlib.pyplot as plot
-data = np.loadtxt(’fichier.txt’)
-Afficher le contenu d’une matrice sous la forme d’une image
 
-plot.figure(1)
-plot.imshow(data, vmin=25, vmax=35)
-plot.colorbar()
-plot.show()
-Quelques fonctions utiles
-scipy.optimize.curve_fit
-numpy.where
+if __name__ == "__main__" :
+    
+    
+    data = np.loadtxt('row_image.dat')
+    plot.figure(1)
+    plot.subplot(2,2,1)
+    plot.imshow(data)
+    
